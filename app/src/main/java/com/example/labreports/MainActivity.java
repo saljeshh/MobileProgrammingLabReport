b20971863feac7c3e759c78d3f37cc7457d0c12a
saljeshh <saljesh2020@gmail.com>
2023-08-05 19:01:50 +0545

    Lab 2 - types of layout

--- app/src/main/java/com/example/labreports/MainActivity.java
+++ app/src/main/java/com/example/labreports/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.labreports;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,33 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.example_menu,menu);
-        return true;
-    }
-
-    // ctrl + o
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        int id = item.getItemId();
-
-        if(id == R.id.item1){
-            Toast.makeText(this,"Item 1 selected", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.item2){
-            Toast.makeText(this,"Item 2 is selected", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.item3){
-            Toast.makeText(this,"Item 3 is selected", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.subitem1){
-            Toast.makeText(this,"Sub Item 1 is selected", Toast.LENGTH_SHORT).show();
-        }else if(id == R.id.subitem2){
-            Toast.makeText(this,"Sub Item 2 is selected", Toast.LENGTH_SHORT).show();
-        }
-
-        return super.onOptionsItemSelected(item);
+        setContentView(R.layout.constraint_layout);
     }
 }
